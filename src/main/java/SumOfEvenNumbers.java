ce28b75dee6b2b6be046efebec38d4f6e3d9f359
BraydenV42 <brayriley44@gmail.com>
2025-10-14 13:29:18 -0400

    Update SumOfEvenNumbers.java

--- src/main/java/SumOfEvenNumbers.java
+++ src/main/java/SumOfEvenNumbers.java
@@ -10,11 +10,11 @@ public class SumOfEvenNumbers {
         number = sc.nextInt();
 
         // TODO: Step 2 - Validate input
-        if(number >= 2){
+        if(number == 2){
             for(int i = 0; i <= number; i += 2) {
                 total += i;
             }
-            System.out.println("The sum of even numbers between 2 and 10 is " + total);
+            System.out.println("The sum of even numbers between 2 and " + number + " is " + total);
         }
         else{
             System.out.println("Error: Input must be 2 or greater.");
